f3bf2bb99e7f36df9caa3240a6b29643a0ea0b9c
David Morgan <djm_uk@protonmail.com>
2025-03-29 14:08:47 +0000

    Initialise with seancorfield/deps-new

---
+++ src/recipe_search_tech_test/recipe_search_tech_test.clj
@@ -0,0 +1,12 @@
+(ns recipe-search-tech-test.recipe-search-tech-test
+  (:gen-class))
+
+(defn greet
+  "Callable entry point to the application."
+  [data]
+  (println (str "Hello, " (or (:name data) "World") "!")))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (greet {:name (first args)}))
